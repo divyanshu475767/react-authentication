ee360f40e47230e13546dc2ff1a77ee92b86840e
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-11-14 10:04:53 +0530

    token expired

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -8,13 +8,14 @@ export const AuthContextProvider = props =>{
 
   const [token , setToken] = useState(initialToken); 
   const userIsLoggedIn = !!token;
-  
+
   const loginHandler = (token)=>{
     setToken(token);
     localStorage.setItem('token', token);
     
   };
 
+   
   const logoutHandler = ()=>{
     setToken(null);
     localStorage.removeItem('token');
@@ -26,11 +27,10 @@ export const AuthContextProvider = props =>{
     isLoggedIn: userIsLoggedIn,
     login:loginHandler,
     logout:logoutHandler,
-  }
-    return 
-    <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
+  };
+  return  <AuthContext.Provider value={contextValue}>{props.children}</AuthContext.Provider>
 
-}
+};
 
 
 export default AuthContext
