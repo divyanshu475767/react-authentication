ab1b738a2f39f6851573883cd6e16e9d519de488
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-11-14 09:45:16 +0530

    local storage

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
-import { AuthContextProvider } from './store/auth-context';
+import { Switch, Route  ,Redirect} from "react-router-dom";
+import { useContext } from "react";
+import Layout from "./components/Layout/Layout";
+import UserProfile from "./components/Profile/UserProfile";
+import AuthPage from "./pages/AuthPage";
+import HomePage from "./pages/HomePage";
+import AuthContext, { AuthContextProvider } from "./store/auth-context";
 
 function App() {
+  const authCtx = useContext(AuthContext);
   return (
     <AuthContextProvider>
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
+      <Layout>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
+          {!authCtx.isloggedIn && (
+            <Route path="/auth">
+              <AuthPage />
+            </Route>
+          )}
+
+          {authCtx.isloggedIn && (
+            <Route path="/profile">
+              <UserProfile />
+            </Route>
+          )}
+          <Route path='*'>
+          <Redirect to='/' />
+          </Route>
+        </Switch>
+      </Layout>
     </AuthContextProvider>
-    
   );
 }
 
